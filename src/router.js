2e6312e5d4d4f7b223c5df5a452d5667c017b0eb
Ari Bambang Kurniawan <aribk210@gmail.com>
2020-12-22 11:03:53 +0700

    add route page components

--- src/router.js
+++ src/router.js
@@ -1,18 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import TeacherDetail from './pages/teachers/TeacherDetail.vue';
+import TeachersList from './pages/teachers/TeachersList.vue';
+import TeacherRegistration from './pages/teachers/TeacherRegistration.vue';
+import ContactTeacher from './pages/requests/ContactTeacher.vue';
+import RequestReceived from './pages/requests/RequestReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/teachers' },
-    { path: '/teachers', component: null },
+    { path: '/teachers', component: TeachersList },
     {
       path: '/teachers/:id',
-      component: null,
-      children: [{ path: '/contact', component: null }],
+      component: TeacherDetail,
+      children: [{ path: '/contact', component: ContactTeacher }],
     },
-    { path: '/register', component: null },
-    { path: '/requests', component: null },
-    { path: '/:notFound(.*)', component: null },
+    { path: '/register', component: TeacherRegistration },
+    { path: '/requests', component: RequestReceived },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
 });
 
